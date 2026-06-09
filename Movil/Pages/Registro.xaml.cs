ffcb3ac632b4efc8250693eb252cc89d7894d715
Ferelan0621 <qwertyxdyono@gmail.com>
2026-06-08 19:07:05 -0600

    limpieza de codigo en movil superficial, y cambios en el api, asi bien aca bien uwu alv padrino

--- Movil/Pages/Registro.xaml.cs
+++ Movil/Pages/Registro.xaml.cs
@@ -11,17 +11,7 @@ public partial class Registro : ContentPage
 		InitializeComponent();
         _apiService = new ApiService();
     }
-    private async void CargarRoles()
-    {
-        // Asumiendo que en tu ApiService tienes un método que le pega al GET de roles
-        // y te devuelve un List<RolDto>
-        var roles = await _apiService.ObtenerRolesAsync();
-
-        if (roles != null)
-        {
-            RolPicker.ItemsSource = roles;
-        }
-    }
+   
     private void Registrar_Clicked(object sender, EventArgs e)
     {
 
